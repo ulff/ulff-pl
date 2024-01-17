56f887712b132a67b5297393ea005df5a1f2b48b
Olaf Galazka <olaf.galazka@schibsted.com>
2024-01-17 21:38:22 +0100

    new social network links, created components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
 
 import myPhoto from "../public/me.jpg";
-
-import xIcon from "../public/social-network/x.svg";
-import facebookIcon from "../public/social-network/facebook.svg";
-import youtubeIcon from "../public/social-network/youtube.svg";
-import githubIcon from "../public/social-network/github.svg";
-import linkedinIcon from "../public/social-network/linkedin.svg";
-
-const iconSize = 128;
+import {SocialNetworkLink} from "@/src/components/SocialNetworkLink";
+import {AboutMeParagraphs} from "@/src/components/AboutMeParagraphs";
 
 export default function Home() {
   return (<>
@@ -31,24 +25,7 @@ export default function Home() {
             />
           </div>
           <div className="about-me-tile about-me-tile-text">
-            <p className="about-me-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p className="about-me-text">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-                sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-                est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
-                modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-                veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-                commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-                nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
+            <AboutMeParagraphs />
           </div>
         </div>
       </div>
@@ -56,46 +33,11 @@ export default function Home() {
       <div className="find-me">
         <h1>&mdash; Where to find me &mdash;</h1>
         <div className="find-me-icons">
-            <Image
-                priority
-                src={githubIcon}
-                height={iconSize}
-                width={iconSize}
-                className="social-network-icon"
-                alt="Find me on GitHub"
-            />
-            <Image
-                priority
-                src={linkedinIcon}
-                height={iconSize}
-                width={iconSize}
-                className="social-network-icon"
-                alt="Find me on LinkedIn"
-            />
-            <Image
-                priority
-                src={facebookIcon}
-                height={iconSize * 0.9}
-                width={iconSize * 0.9}
-                className="social-network-icon"
-                alt="Find me on Facebook"
-            />
-            <Image
-                priority
-                src={youtubeIcon}
-                height={iconSize}
-                width={iconSize}
-                className="social-network-icon"
-                alt="Find me on YouTube"
-            />
-            <Image
-                priority
-                src={xIcon}
-                height={iconSize * 0.7}
-                width={iconSize * 0.7}
-                className="social-network-icon"
-                alt="Find me on X"
-            />
+          <SocialNetworkLink service="github" serviceFullName="GitHub" />
+          <SocialNetworkLink service="linkedin" serviceFullName="LinkedIn" />
+          <SocialNetworkLink service="facebook" serviceFullName="Facebook" />
+          <SocialNetworkLink service="youtube" serviceFullName="YouTube" />
+          <SocialNetworkLink service="x" serviceFullName="X (Twitter)" />
         </div>
       </div>
   </>)
---
+++ src/components/SocialNetworkLink.tsx
@@ -0,0 +1,46 @@
+import React, {ReactElement} from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import githubIcon from "../../public/social-network/github.svg";
+import linkedinIcon from "../../public/social-network/linkedin.svg";
+import facebookIcon from "../../public/social-network/facebook.svg";
+import youtubeIcon from "../../public/social-network/youtube.svg";
+import xIcon from "../../public/social-network/x.svg";
+
+type SocialNetwork = "github" | "linkedin" | "facebook" | "youtube" | "x";
+
+const socialNetworkLinks: { [key in SocialNetwork]: string } = {
+  github: "https://github.com/ulff",
+  linkedin: "https://www.linkedin.com/in/olaf-ga%C5%82%C4%85zka-40301485/",
+  facebook: "https://www.facebook.com/olaf.galazka",
+  youtube: "https://www.youtube.com/@olafgalazka6379",
+  x: "https://twitter.com/olafgalazka",
+}
+
+const socialNetworkIcons: { [key in SocialNetwork]: any } = {
+  github: githubIcon,
+  linkedin: linkedinIcon,
+  facebook: facebookIcon,
+  youtube: youtubeIcon,
+  x: xIcon,
+}
+
+interface Props {
+  service: SocialNetwork;
+  serviceFullName: string;
+}
+
+export const SocialNetworkLink: React.FC<Props> = ({ service, serviceFullName }: Props): ReactElement => {
+  return <Link href={socialNetworkLinks[service]} target="_blank">
+    <Image
+      priority
+      src={socialNetworkIcons[service]}
+      height={128}
+      width={128}
+      className="social-network-icon"
+      alt={`Find me on ${serviceFullName}`}
+      title={`Find me on ${serviceFullName}`}
+    />
+  </Link>;
+}
